875fc41b9fb3d7656afceaf976499cee247825da
Kelvin Ben <kevoben14@gmail.com>
2022-11-02 20:18:21 +0300

    Create a component consisting of 3 pages: Home, Calculator, and Quote

---
+++ src/pages/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <>
+      <h2 className="welcome">Welcome to our Page!</h2>
+      <p className="para1">
+        Lorem ipsum dolor sit amet, consectetur adipiscing sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+      <p className="para1">
+        Lorem ipsum dolor sit amet, consectetur adipiscing sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+    </>
+  );
+}
---
+++ src/pages/Quote.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Quote() {
+  return (
+    <div>
+      <p className="quote">
+        Mathematics is not about number,equations,computations,
+        or algorithms: it is about understanding. -William Paul Thurston
+      </p>
+    </div>
+  );
+}
